d15142ec34e444916cead011b30ab731ee0bc61e
Connor Tangney <Couch3ater@gmail.com>
2018-04-24 12:55:14 -0400

    build out data structure; parse airline names with claims > 0 value

--- app/test.js
+++ app/test.js
@@ -1,18 +1,30 @@
 addEventListener('message', function(e) {
-  //storage for claims data
   var claims = e.data;
-  //var linePlotData = new Map();
+  var airlines = new Array();
+  var months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+  var claimDate, claimAmount;
+
+  //final storage
   var linePlotData = [];
 
-  //below code will loop through the properties (keys) of the first claim
-  /*for (property in claims[0]){
-  	linePlotData += property;
-  }*/
+  /*
+    - iterate through data
+    - if the airline on the current row has not yet been added to the outer-most data store, insert ["Airline Name"] property
+    - determine which month the claim was received using ["Date Received"] property and the months Array
+    - convert ["Close Amount"] to floating point
+    - add ["Close Amount"] value to correct cell: [claim][month-array-index]
 
-  //the below code converts the papaparse object to a Map
-  /*Object.keys(claims).forEach(key => {
-  	linePlotData.set(key, claims[key]);
-  });*/
+    ! checking for 0 value claims during iteration
+  */
+  for(claim in claims){
+    claimAmount = Number(claims[claim]["Close Amount"].replace(/[^0-9\.-]+/g,""));
+    if(claimAmount != 0 && !(isNaN(claimAmount))){
+      claimDate = claims[claim]["Date Received"].split('-')[1];
+      if(!(linePlotData.includes(claims[claim]["Airline Name"].trim()))){
+        linePlotData.push(claims[claim]["Airline Name"].trim());
+      }
+    }
+  }
 
   //return message to main
   postMessage(linePlotData);
