4919c683376e6ebccdae70ba57af9f984480dd00
Connor <couch3ater@gmail.com>
2018-04-24 06:27:52 -0400

    added a code snippet to show how to access papaparse object 'key'

--- app/test.js
+++ app/test.js
@@ -2,7 +2,12 @@ addEventListener('message', function(e) {
   //storage for claims data
   var claims = e.data;
   //var linePlotData = new Map();
-  var linePlotData = new Array(claims.length);
+  var linePlotData = [];
+
+  //below code will loop through the properties (keys) of the first claim
+  /*for (property in claims[0]){
+  	linePlotData += property;
+  }*/
 
   //the below code converts the papaparse object to a Map
   /*Object.keys(claims).forEach(key => {
